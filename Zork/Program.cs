8bfe2468a9fb341b405a37277638d65a782a8a6a
Carattiniw <45584185+Suddenrebel@users.noreply.github.com>
2020-09-10 18:55:26 -0400

    Zork 1.3
    The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,28 +2,48 @@
 
 namespace Zork
 {
-    enum Commands
-    {
-        QUIT,
-        LOOK,
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST,
-        UNKNOWN
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA Rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                        outputString = "You moved NORTH.";
+                        break;
+                    case Commands.SOUTH:
+                        outputString = "You moved SOUTH.";
+                        break;
+                    case Commands.EAST:
+                        outputString = "You moved EAST.";
+                        break;
+                    case Commands.WEST:
+                        outputString = "You moved WEST.";
+                        break;
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
