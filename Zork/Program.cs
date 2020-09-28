5371668b4ffe5ea9eb42d20400675681c59eba01
Carattiniw <45584185+Suddenrebel@users.noreply.github.com>
2020-09-28 14:20:26 -0400

    Zork 3.1
    Object-Oriented Rooms

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,7 +6,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        public static Room CurrentRoom
         {
             get
             {
@@ -14,9 +14,12 @@ namespace Zork
             }
         }
 
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
@@ -32,7 +35,7 @@ namespace Zork
                         break;
 
                     case Commands.LOOK:
-                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
@@ -86,19 +89,56 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static string[,] Rooms =
+        private static readonly Room[,] Rooms =
         {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Woods", "North of House", "Clearing" }
+            //what is shown in 3.1
+            //{ new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            //{ new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            //{ new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+
+            //below matches output example for 3.1
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") }
         };
+
+        private static void InitializeRoomDescriptions()
+        {
+            //what is shown in 3.1
+            //Rooms[0, 0].Description = "You are on a rock-strewn trail.";                                                                                                        //Rocky Trail
+            //Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";                                 //South of House
+            //Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                                              //
+
+            //Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                                             //Forest
+            //Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                                                //West of House
+            //Rooms[1, 2].Description = "You are behind the white house.  In one corner of the house there is a small window which is a small window which is slightly ajar.";    //Behind House
+
+            //Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around.  To the east, there appears to be sunlight.";                                   //Dense Woods
+            //Rooms[2, 1].Description = "You are facing the north side of a white house.  There is no door here, and all the windows are barred.";                                //North of House
+            //Rooms[2, 2].Description = "You are in a clearing, with a forest surroudning you on the west and south.";                                                            //Clearing
+
+
+            //below matches 3.1 output example
+            Rooms[0, 0].Description = "This is a dimly lit forest, with large trees all around.  To the east, there appears to be sunlight.";                                   //Dense Woods
+            Rooms[0, 1].Description = "You are facing the north side of a white house.  There is no door here, and all the windows are barred.";                                //North of House
+            Rooms[0, 2].Description = "You are in a clearing, with a forest surroudning you on the west and south.";                                                            //Clearing
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                                             //Forest
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                                                //West of House
+            Rooms[1, 2].Description = "You are behind the white house.  In one corner of the house there is a small window which is a small window which is slightly ajar.";    //Behind House
+
+            Rooms[2, 0].Description = "You are on a rock-strewn trail.";                                                                                                        //Rocky Trail
+            Rooms[2, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";                                 //South of House
+            Rooms[2, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                                              //
+        }
 
         private static readonly List<Commands> Directions = new List<Commands>
         {
             Commands.NORTH,
             Commands.SOUTH,
             Commands.EAST,
-            Commands.WEST
+            Commands.WEST,
+            Commands.LOOK
         };
 
         private static (int Row, int Column) Location = (1, 1); //sets the current location to West of House and feeds it to CurrentRoom
